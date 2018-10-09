0c367390502be0898e93c0d608d205ad7c23a7d7
Wuriqilang <543677991@qq.com>
2018-10-09 16:30:18 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,9 +1,10 @@
 #coding=utf-8
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
-import requests
 
-resp=requests.get('https://www.baidu.com') #请求百度首页
-#print resp #打印请求结果的状态码
-print resp.content #打印请求到的网页源码
+import requests
+from urllib import request
+
+if __name__=='__main__':
+    response=request.urlopen("https://www.baidu.com")
+    html=response.read()
+    print(html)
+
